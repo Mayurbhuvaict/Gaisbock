23efedc628b56842a81eb33944c840d897e17e3c
rutik <rutik@icreativetechnolabs.com>
2023-07-26 19:15:28 +0530

    Listing page Heading element

---
+++ src/Resources/app/administration/src/module/sw-cms/blocks/gaisbockOption/gaisbock-listpage-text-image/component/index.js
@@ -0,0 +1,5 @@
+import template from './sw-cms-block-gaisbock-listpage-text-image.html.twig';
+
+export default {
+    template
+}
---
+++ src/Resources/app/administration/src/module/sw-cms/blocks/gaisbockOption/gaisbock-listpage-text-image/index.js
@@ -0,0 +1,36 @@
+import CMS from "../../../constant/sw-cms.constant";
+
+Shopware.Component.register('sw-cms-block-gaisbock-listpage-text-image',()=>import('./component'));
+Shopware.Component.register('sw-cms-preview-gaisbock-listpage-text-image',()=>import('./preview'));
+
+Shopware.Service('cmsService').registerCmsBlock({
+    name: 'gaisbock-listpage-text-image',
+    label: 'sw-cms.blocks.ictTextImage.ictSwitchImageTextBubble.label',
+    category: 'gaisbock-cms-elements',
+    component: 'sw-cms-block-gaisbock-listpage-text-image',
+    previewComponent: 'sw-cms-preview-gaisbock-listpage-text-image',
+    allowedPageTypes:['product_list'],
+    defaultConfig: {
+        marginBottom: '20px',
+        marginTop: '20px',
+        marginLeft: '20px',
+        marginRight: '20px',
+        sizingMode: 'boxed',
+    },
+    slots: {
+        gaisbockListpageImageText: {
+            type: 'gaisbock-listpage-text-image',
+            default: {
+                config: {
+                    displayMode: { source: 'static', value: 'standard' },
+                },
+                data: {
+                    media: {
+                        value: CMS.MEDIA.previewMountain,
+                        source: 'default',
+                    },
+                },
+            },
+        },
+    },
+});
