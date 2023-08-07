655ec06d47ddcd95ba9045a37971d7adfc605446
rutik <rutik@icreativetechnolabs.com>
2023-08-07 17:08:15 +0530

    add new element

---
+++ src/Resources/app/administration/src/module/sw-cms/blocks/gaisbockOption/gaisbock-about-title-text-image/component/index.js
@@ -0,0 +1,5 @@
+import template from './sw-cms-block-gaisbock-about-title-text-image.html.twig';
+import './sw-cms-block-gaisbock-about-title-text-image.scss';
+export default {
+    template,
+}
---
+++ src/Resources/app/administration/src/module/sw-cms/elements/gaisbock-about-title-text-image/preview/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-el-preview-gaisbock-about-title-text-image.html.twig';
+import './sw-cms-el-preview-giasbock-about-title-text-image.scss';
+
+export default {
+    template
+}
