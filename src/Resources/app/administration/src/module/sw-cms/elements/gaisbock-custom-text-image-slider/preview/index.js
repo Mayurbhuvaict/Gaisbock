14033f69cd13f387eb4c95c9222019b4dfa35acc
rutik <rutik@icreativetechnolabs.com>
2023-08-08 16:45:44 +0530

    Addmore element with image slider

---
+++ src/Resources/app/administration/src/module/sw-cms/elements/gaisbock-custom-text-image-slider/preview/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-el-preview-gaisbock-custom-text-image-slider.html.twig';
+import './sw-cms-el-preview-gaisbock-custom-text-image-slider.scss';
+
+export default {
+    template
+}
--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,8 +1,10 @@
 import gaisbockAddmoreSection from './js/gaisbock-addmore-section.plugin';
 import gaisbockAddRemoveClass from './js/gaisbock-add-remove-class.plugin';
 import gaisbockThemeScrollSection from './js/gaisbock-theme-scroll-section-plugin';
+import gaisbockimageslider from "./js/gaisbock-image-slider.plugin";
 
 const PluginManager = window.PluginManager;
 PluginManager.register('gaisbockAddmoreSection',gaisbockAddmoreSection,'[data-gaisbock-addmore-section]');
 PluginManager.register('gaisbockAddRemoveClass',gaisbockAddRemoveClass,'[data-gaisbock-add-remove-class]');
 PluginManager.register('gaisbockThemeScrollSection',gaisbockThemeScrollSection,'[data-gaisbock-scroll-section]');
+PluginManager.register('gaisbockimageslider',gaisbockimageslider,'[data-gaisbock-image-slider]')
