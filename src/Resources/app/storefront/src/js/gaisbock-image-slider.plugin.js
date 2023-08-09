e5bc8cd71acef10546eca93c1aa32960a56ec1b5
rutik <rutik@icreativetechnolabs.com>
2023-08-09 11:05:03 +0530

    add js

---
+++ src/Resources/app/storefront/src/js/gaisbock-image-slider.plugin.js
@@ -0,0 +1,23 @@
+import Plugin from 'src/plugin-system/plugin.class';
+
+export default class gaisbockimageslider extends Plugin {
+    init() {
+        var classname = document.getElementById("gaisbock-slider");
+        console.log(classname)
+        if (classname !== null) {
+            var slider = {
+                slider: {
+                    navPosition: 'bottom',
+                    speed: 300,
+                    autoplayTimeout: 5000,
+                    autoplay: false,
+                    autoplayButtonOutput: false,
+                    nav: true,
+                    controls: true,
+                    autoHeight: true
+                }
+            };
+            classname.setAttribute('data-base-slider-options',JSON.stringify(slider));
+        }
+    }
+}
