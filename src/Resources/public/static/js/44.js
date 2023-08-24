36b51695f198f6d65b29855133f030c09e63de7a
rutik <rutik@icreativetechnolabs.com>
2023-08-24 12:50:01 +0530

    add js build

---
+++ src/Resources/public/static/js/44.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpPlugingaisbock=this.webpackJsonpPlugingaisbock||[]).push([[44],{P2Wz:function(t,n,s){"use strict";s.r(n);n.default={template:'\n{% block sw_cms_block_gaisbock_text %}\n<div class="sw-cms-block-gaisbock-text">\n    <slot name="content">\n        \n        {% block sw_cms_block_text_slot_text_content %}{% endblock %}\n    </slot>\n</div>\n{% endblock %}\n'}}}]);
---
+++ src/Resources/public/static/js/52.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpPlugingaisbock=this.webpackJsonpPlugingaisbock||[]).push([[52],{o3ye:function(e,s,i){"use strict";i.r(s);s.default={template:"{% block sw_cms_el_preview_gaisbock_image_add_more_text %}\n{% endblock %}"}}}]);
