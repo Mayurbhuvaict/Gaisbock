7848934dd64ada4533137b70c01063ffadb4e667
sagar1ladani <Parthiv@icreativetechnolabs.com>
2023-08-28 14:09:14 +0530

    animation

--- src/Resources/app/storefront/src/js/gaisbock-theme-scroll-section-plugin.js
+++ src/Resources/app/storefront/src/js/gaisbock-theme-scroll-section-plugin.js
@@ -3,6 +3,19 @@ import Plugin from 'src/plugin-system/plugin.class';
 export default class gaisbockThemeScrollSection extends Plugin {
     init() {
         this._gaisbockThemeScroll();
+        const overlay = document.querySelector('.cms-block-gaisbock-custom-text-on-image');
+        const content = document.querySelector('.static-banner-section');
+
+        window.addEventListener('scroll', () => {
+        // Calculate scroll progress
+        const scrollProgress = (window.scrollY / content.clientHeight);
+
+        // Calculate opacity based on scroll progress
+        const overlayOpacity = Math.min(0.7, scrollProgress * 0.7);
+
+        // Update overlay background color
+        overlay.style.backgroundColor = `rgba(0, 0, 0, ${overlayOpacity})`;
+        });
     }
 
     _gaisbockThemeScroll() {
