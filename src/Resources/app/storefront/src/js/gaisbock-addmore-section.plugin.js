cdcb8eceb98a4d3a8a7741454ea71f2572a0aa97
rutik <rutik@icreativetechnolabs.com>
2023-09-01 15:53:52 +0530

    Change image slider JS

--- src/Resources/app/storefront/src/js/gaisbock-addmore-section.plugin.js
+++ src/Resources/app/storefront/src/js/gaisbock-addmore-section.plugin.js
@@ -15,15 +15,13 @@ export default class gaisbockAddmoreSection extends Plugin {
                 const id = button.getAttribute("data-diffrent");
                 var textDiv = document.getElementById("gaisbock-addmore-section-"+id);
                 button.addEventListener("click", function () {
+                    textDiv.style.display = textDiv.style.display === "block" ? "none" : "block";
                     if (language === "de-DE") {
                         button.textContent = button.textContent === "Mehr lesen" ? "Weniger lesen" : "Mehr lesen";
-                        textDiv.style.display = textDiv.style.display === "block" ? "none" : "block";
                     } else if(language === "en-GB") {
                         button.textContent = button.textContent === "Read More" ? "Read Less" : "Read More";
-                        textDiv.style.display = textDiv.style.display === "block" ? "none" : "block";
                     }else {
                         button.textContent = button.textContent === "Lire la suite" ? "Lire moins" : "Lire la suite";
-                        textDiv.style.display = textDiv.style.display === "block" ? "none" : "block";
                     }
                 });
             });
