779da260be088558303201c3ef1860789dff7d9e
Rohit Parmar <rohit@icreativetechnologies.com>
2023-10-16 07:37:04 +0200

    Remove unwanted code

--- custom/plugins/ICTECHNewsletterWithPromotion/src/Resources/app/administration/src/main.js
+++ custom/plugins/ICTECHNewsletterWithPromotion/src/Resources/app/administration/src/main.js
@@ -1,4 +1,4 @@
-console.log('newsletter called');
+console.log('newsletter-called');
 import './module/ict-newsletter-with-promotion';
 import './component/newsletter-popup-info-config-field';
 import './extension/sw-promotion-v2-individual-codes-beahvior';
